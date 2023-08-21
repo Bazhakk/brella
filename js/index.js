86ad2511162cf182784a8d01196e5c46164949c6
unknown <bazhakk@gmail.com>
2023-08-21 16:40:44 +0300

    fix

---
+++ js/index.js
@@ -0,0 +1,31 @@
+//Переключатель языка
+// document.addEventListener("DOMContentLoaded", function () {
+//     const languageSelect = document.getElementById("language-select");
+//     const greetingElement = document.getElementById("greeting");
+  
+//     // Обработчик изменения выбора языка
+//     languageSelect.addEventListener("change", function () {
+//       const selectedLanguage = languageSelect.value;
+//       updateLanguage(selectedLanguage);
+//     });
+  
+//     // Функция обновления текста на выбранном языке
+//     function updateLanguage(language) {
+//       switch (language) {
+//         case "en":
+//           greetingElement.textContent = "Hello, world!";
+//           break;
+//         case "es":
+//           greetingElement.textContent = "¡Hola, mundo!";
+//           break;
+//         case "fr":
+//           greetingElement.textContent = "Bonjour, le monde !";
+//           break;
+//         // Добавьте другие языки по необходимости
+//         default:
+//           greetingElement.textContent = "Hello, world!";
+//           break;
+//       }
+//     }
+// });
+  
